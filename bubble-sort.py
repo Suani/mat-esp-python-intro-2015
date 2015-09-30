0f71991aa602e3e90e5bda8e1ab41b4afd6e259b
gustavo <abrahim.gustavo@gmail.com>
2015-09-30 15:22:04 -0300

    arquivo com comando para print de todas as trocas

---
+++ bubble-sort.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+N = 20
+lista = [11, 18, 3, 1, 16, 12, 6, 19, 5, 0, 14, 4, 17, 9, 13, 7, 10, 15, 2, 8]
+print("lista original", lista) 
+plt.figure()
+plt.plot(range(0, N), lista, "ok")
+plt.title("estado inicial")
+plt.xlabel("indices")
+plt.ylabel("valores")
+plt.savefig("fig/bubble-inicio.png")
+plt.close()
+a=0
+# considerando i numeros da lista comecando na posicao 0 e terminando na posicao N-1, indo de 1 a 1 
+for i in range(0, N-1, 1):
+# considerando j numeros da lista comecando em uma posicao a frente do i e terminando no numero da ultima posicao da lista, indo de 1 a 1
+     for j in range(i + 1, N, 1):
+# comparando o valor de i com j, caso i seja maior que j, entao seguir os passos seguintes
+        if lista[i] > lista[j]:
+            temp = lista[i]
+            lista[i] = lista[j]
+            lista[j] = temp
+            a=a+1
+            plt.figure() 
+            plt.plot (range(0, N), lista, "ok")
+            plt.title("bubble-troca-{}".format(a))
+            plt.xlabel("indices")
+            plt.ylabel("valores")
+            plt.savefig("fig/bubble-troca-{}.png".format(a))
+            plt.close()
+print("lista ordem crescente", lista)
+plt.figure()
+plt.plot (range(0, N), lista, "ok")
+plt.title("estado final")
+plt.xlabel("indices")
+plt.ylabel("valores")
+plt.savefig("fig/bubble-final.png")
+plt.close()
+print("cinco maiores valores", lista[N-5:N])
+print("cinco menores valores", lista[0:5])
+# organizando a lista com valores em ordem crescente, lista com cinco menores e 5 maiores
